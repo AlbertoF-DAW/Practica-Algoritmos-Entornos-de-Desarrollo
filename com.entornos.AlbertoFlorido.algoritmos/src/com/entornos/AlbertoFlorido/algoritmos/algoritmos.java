2d24293093133370de1a305cf5367d9c3c71080e
AlbertoF-DAW <aff0017@alu.medac.es>
2023-03-27 10:48:30 +0200

    Funcion factorial hecha

--- com.entornos.AlbertoFlorido.algoritmos/src/com/entornos/AlbertoFlorido/algoritmos/algoritmos.java
+++ com.entornos.AlbertoFlorido.algoritmos/src/com/entornos/AlbertoFlorido/algoritmos/algoritmos.java
@@ -5,8 +5,8 @@ public abstract class algoritmos {
 	/**
 	 * Recibe un numero entero y devuelve el numero de fibonacci de dicho numero.
 	 * 
-	 * @param n El numero cuyo fibonacci calcular
-	 * @return El valor de la sucesion de fibonacci en la posicion N-esima
+	 * @param n El numero cuyo fibonacci se quiere calcular
+	 * @return El valor de la sucesion de fibonacci en la posicion n-esima
 	 */
 	public static int fibonacci(int n) {
 		int n0 = 0, n1 = 1, res = 0;
@@ -19,6 +19,20 @@ public abstract class algoritmos {
 				n1 += n0;
 				res = n1;
 			}
+		}
+		return res;
+	}
+
+	/**
+	 * Recibe un numero entero y devuelve el factorial de dicho numero.
+	 * 
+	 * @param n El numero cuyo factorial se quiere calcular
+	 * @return El valor del factorial de n
+	 */
+	public static int factorial(int n) {
+		int res = 1;
+		for (int i = 2; i <= n; i++) {
+			res *= i;
 		}
 		return res;
 	}
