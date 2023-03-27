5d1cd6f885647b7fd56fb8919988e55ee757e6a2
AlbertoF-DAW <aff0017@alu.medac.es>
2023-03-27 11:03:31 +0200

    Funcion primo hecha, y cambio algunos retornos a long

--- com.entornos.AlbertoFlorido.algoritmos/src/com/entornos/AlbertoFlorido/algoritmos/algoritmos.java
+++ com.entornos.AlbertoFlorido.algoritmos/src/com/entornos/AlbertoFlorido/algoritmos/algoritmos.java
@@ -1,5 +1,10 @@
 package com.entornos.AlbertoFlorido.algoritmos;
 
+/**
+ * 
+ * @author Alberto
+ *
+ */
 public abstract class algoritmos {
 
 	/**
@@ -8,8 +13,9 @@ public abstract class algoritmos {
 	 * @param n El numero cuyo fibonacci se quiere calcular
 	 * @return El valor de la sucesion de fibonacci en la posicion n-esima
 	 */
-	public static int fibonacci(int n) {
-		int n0 = 0, n1 = 1, res = 0;
+	public static long fibonacci(int n) {
+		int n0 = 0, n1 = 1;
+		long res = 0;
 
 		for (int i = 0; i < 0; i++) {
 			if (i % 2 == 0) {
@@ -29,11 +35,27 @@ public abstract class algoritmos {
 	 * @param n El numero cuyo factorial se quiere calcular
 	 * @return El valor del factorial de n
 	 */
-	public static int factorial(int n) {
-		int res = 1;
+	public static long factorial(int n) {
+		long res = 1;
 		for (int i = 2; i <= n; i++) {
 			res *= i;
 		}
 		return res;
+	}
+
+	/**
+	 * Recibe un numero entero y devuelve true o false en funcion de si es primo o
+	 * no.
+	 * 
+	 * @param n El numero entero a comprobar si es primo
+	 * @return Devuelve un valor booleano segun si es primo (true) o no (false)
+	 */
+	public static boolean primo(int n) {
+		for (int i = 2; i <= (int) Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
